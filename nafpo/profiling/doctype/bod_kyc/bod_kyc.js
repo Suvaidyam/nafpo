8973bc95a20f0f886783045153525cd629f9e4d8
Abhishek kumar <abhishek.suvaidyam@gmail.com>
2024-05-21 14:54:37 +0530

    Add changes

--- nafpo/profiling/doctype/bod_kyc/bod_kyc.js
+++ nafpo/profiling/doctype/bod_kyc/bod_kyc.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2024, dhwaniris and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("BOD KYC", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("BOD KYC", {
+	async refresh(frm) {
+        await hide_advance_search(frm, ['state_name', 'fpo_name'])
+        await extend_options_length(frm, ['fpo_name', 'fpo_name'])
+        await apply_filter('fpo_name', 'state', frm, frm.doc.state_name)
+	},
+    state_name:async function(frm){
+        await apply_filter('fpo_name', 'state', frm, frm.doc.state_name)
+        truncate_multiple_fields_value(frm, ['fpo_name'])
+    }
+});
