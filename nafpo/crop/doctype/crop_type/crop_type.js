3aa0e76c9c89965b9dd0965cfb9fed552e5c2815
Samar Raj <samar.suvaidyam@gmail.com>
2024-05-21 09:43:44 +0530

    changes in master module

--- nafpo/crop/doctype/crop_type/crop_type.js
+++ nafpo/crop/doctype/crop_type/crop_type.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2024, dhwaniris and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Crop Type", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Crop Type", {
+    refresh(frm) {
+        hide_advance_search(frm, ['state_name', 'fpo'])
+        extend_options_length(frm, ['state_name', 'fpo'])
+        apply_filter('fpo', 'state', frm, frm.doc.state_name)
+    },
+    state_name: function (frm) {
+        apply_filter('fpo', 'state', frm, frm.doc.state_name)
+        truncate_multiple_fields_value(frm, ['fpo'])
+    }
+});
