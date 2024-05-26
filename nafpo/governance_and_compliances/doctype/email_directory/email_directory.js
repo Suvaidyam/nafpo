fb01d548da5977ea4f70dd8f9a85e2488c13f79b
Samar Raj <samar.suvaidyam@gmail.com>
2024-05-26 10:52:00 +0530

    changes in gov compliance module

--- nafpo/governance_and_compliances/doctype/email_directory/email_directory.js
+++ nafpo/governance_and_compliances/doctype/email_directory/email_directory.js
@@ -1,8 +1,9 @@
 // Copyright (c) 2024, dhwaniris and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Email Directory", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Email Directory", {
+    refresh(frm) {
+        hide_advance_search(frm, ['fpo_name'])
+        extend_options_length(frm, ['fpo_name'])
+    }
+});
