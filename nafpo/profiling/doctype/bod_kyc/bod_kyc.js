69e30b58b150c746c5e268c4fd408bcf0837d120
Abhishek kumar <abhishek.suvaidyam@gmail.com>
2024-06-02 12:03:00 +0530

    add

--- nafpo/profiling/doctype/bod_kyc/bod_kyc.js
+++ nafpo/profiling/doctype/bod_kyc/bod_kyc.js
@@ -4,7 +4,7 @@
 frappe.ui.form.on("BOD KYC", {
 	async refresh(frm) {
         await hide_advance_search(frm, ['state_name', 'fpo_name'])
-        await extend_options_length(frm, ['fpo_name', 'fpo_name'])
+        await extend_options_length(frm, ['state_name', 'fpo_name'])
         await apply_filter('fpo_name', 'state', frm, frm.doc.state_name)
 	},
     state_name:async function(frm){
