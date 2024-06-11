8bb99534c4442bca5c25e2efa9b0370dc48a3577
Abhishek Yadav <abhi.suvaidyam@gmail.com>
2024-06-11 11:27:08 +0530

    add

--- nafpo/masters/doctype/district/district.js
+++ nafpo/masters/doctype/district/district.js
@@ -5,6 +5,6 @@ frappe.ui.form.on("District", {
     refresh(frm) {
         hide_advance_search(frm, ['state'])
         extend_options_length(frm, ['state'])
-        frm.is_new() && hide_print_button(frm)
+        frm.is_new() ? hide_print_button(frm) : show_print_button(frm);
     },
 });
