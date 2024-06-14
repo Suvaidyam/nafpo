8fcacc2badde8d3985f349f2a7e36078ce4f36bf
Abhishek Yadav <abhi.suvaidyam@gmail.com>
2024-06-14 17:11:55 +0530

    Hide comment , like icon and sidebar

---
+++ nafpo/masters/doctype/cbbo/cbbo_list.js
@@ -0,0 +1,23 @@
+
+frappe.listview_settings['CBBO'] = {
+    refresh: function (listview) {
+        $('use.like-icon').hide();
+        $('.list-row-activity').hide();
+        $(".comment-count").hide();
+        $(".frappe-timestamp").hide();
+        $(".avatar-small").hide()
+        $(".comment-input-wrapper").hide()
+        $(".comment-input-header").hide()
+        $("ql-editor").hide()
+        $("ql-blank").hide()
+    },
+    onload: function (listview) {
+        $('.layout-side-section').hide();
+        $('.sidebar-section.filter-section').hide();
+        $('.sidebar-section.save-filter-section').hide();
+        $(".comment-input-wrapper").hide()
+        $(".comment-input-header").hide()
+        $("ql-editor").hide()
+        $("ql-blank").hide()
+    }
+};
