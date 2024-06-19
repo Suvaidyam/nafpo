224669927bf1fef00a2d65ee96b2d405e367331e
Abhishek Yadav <abhi.suvaidyam@gmail.com>
2024-06-19 15:28:31 +0530

    add

--- nafpo/profiling/doctype/bod_kyc/bod_kyc.js
+++ nafpo/profiling/doctype/bod_kyc/bod_kyc.js
@@ -7,9 +7,19 @@ frappe.ui.form.on("BOD KYC", {
         await extend_options_length(frm, ['state_name', 'fpo_name'])
         await apply_filter('fpo_name', 'state', frm, frm.doc.state_name)
     },
+    validate(frm) {
+        validate_numeric_field(frm, 'mobile_number', 'Mobile Number');
+        validate_numeric_field(frm, 'aadhar_number', 'Aadhar Number');
+    },
     state_name: async function (frm) {
         await apply_filter('fpo_name', 'state', frm, frm.doc.state_name)
         truncate_multiple_fields_value(frm, ['fpo_name'])
+    },
+    mobile_number(frm) {
+        validate_numeric_field(frm, 'mobile_number', 'Mobile Number');
+    },
+    aadhar_number(frm) {
+        validate_numeric_field(frm, 'aadhar_number', 'Aadhar Number');
     },
     onload(frm) {
         hide_list_view_in_useless_data(frm)
@@ -22,4 +32,10 @@ frappe.ui.form.on("BOD KYC", {
     }, {})
 });
 
-
+function validate_numeric_field(frm, field_name, field_label) {
+    const field_value = frm.doc[field_name];
+    if (typeof field_value === 'string' && isNaN(Number(field_value))) {
+        frappe.throw(__(`Please enter a valid ${field_label}.`));
+        frm.set_value(field_name, '');
+    }
+}
--- nafpo/profiling/doctype/fpo_profiling/fpo_profiling.py
+++ nafpo/profiling/doctype/fpo_profiling/fpo_profiling.py
@@ -3,6 +3,5 @@ import frappe
 from frappe.model.document import Document
 
 class FPOProfiling(Document):
-    def before_save(self):
-        fpo_profile_name = frappe.get_doc('FPO', self.name_of_the_fpo)
-        self.name_of_the_fpo_copy = fpo_profile_name.fpo_name
+    pass
+
