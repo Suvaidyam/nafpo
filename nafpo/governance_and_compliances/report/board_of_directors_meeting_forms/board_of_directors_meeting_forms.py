9c321fa89aeb548c2de4f646f72274da231a1724
Abhishek Yadav <abhi.suvaidyam@gmail.com>
2024-06-22 10:15:17 +0530

    delete workspace

--- nafpo/governance_and_compliances/report/board_of_directors_meeting_forms/board_of_directors_meeting_forms.py
+++ nafpo/governance_and_compliances/report/board_of_directors_meeting_forms/board_of_directors_meeting_forms.py
@@ -1,9 +1,39 @@
-# Copyright (c) 2024, dhwaniris and contributors
-# For license information, please see license.txt
-
-# import frappe
-
+import frappe
 
 def execute(filters=None):
-	columns, data = [], []
-	return columns, data
+    columns = [
+        {
+            "fieldname": "name",
+            "label": "Name",
+            "fieldtype": "Data",
+            "width": 400
+        },
+        {
+            "fieldname": "pending_count",
+            "label": "Pending",
+            "fieldtype": "Int",
+            "width": 300
+        },
+        {
+            "fieldname": "completed",
+            "label": "Completed",
+            "fieldtype": "Int",
+            "width": 300
+        },
+    ]
+
+    sql_query = """
+        SELECT
+            'Board of Directors Meeting Forms' AS name,
+            COUNT(CASE WHEN status != 'Completed' THEN 1 END) AS pending_count,
+            CASE 
+                WHEN COUNT(CASE WHEN status = 'Completed' THEN 1 END) >= 4 
+                THEN 'Completed' 
+                ELSE 'Not Completed' 
+            END AS completed
+        FROM
+            `tabBoard of Directors Meeting Forms`
+    """
+
+    data = frappe.db.sql(sql_query, as_dict=True)
+    return columns, data
