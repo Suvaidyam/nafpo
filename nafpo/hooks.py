4db9e6f8e135cdaa8f693a25ec77b1029fe4977f
aniketsuvaidyam <aniket.suvaidyam.com>
2024-06-25 18:51:16 +0530

    nav logo incrage in costom

--- nafpo/hooks.py
+++ nafpo/hooks.py
@@ -19,11 +19,11 @@ fixtures = [
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/nafpo/css/nafpo.css"
+app_include_css = "/assets/nafpo/css/nafpo.css"
 # app_include_js = "/assets/nafpo/js/nafpo.js"
 
 # include js, css files in header of web template
-# web_include_css = "/assets/nafpo/css/nafpo.css"
+web_include_css = "/assets/nafpo/css/nafpo.css"
 # web_include_js = "/assets/nafpo/js/nafpo.js"
 
 # include custom scss in every website theme (without file extension ".scss")
