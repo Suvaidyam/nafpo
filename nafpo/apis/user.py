2102d75b296065e35ffec6b7c890c54830bf6178
Abhishek Yadav <abhi.suvaidyam@gmail.com>
2024-06-25 15:31:37 +0530

    add

--- nafpo/apis/user.py
+++ nafpo/apis/user.py
@@ -9,6 +9,9 @@ def get_user_permission(user, join_con=[]):
                 WHEN UP.allow = 'IA' THEN IA.name1
                 WHEN UP.allow = 'CBBO' THEN CBBO.name_of_cbbo
                 WHEN UP.allow = 'FPO' THEN FPO.fpo_name
+                WHEN UP.allow = 'State' THEN TS.state_name
+                WHEN UP.allow = 'District' THEN TD.district_name
+                WHEN UP.allow = 'Block' THEN TB.block_name
             END AS name_value,
             UP.for_value,
             UP.name,
@@ -18,6 +21,9 @@ def get_user_permission(user, join_con=[]):
         LEFT JOIN `tabIA` AS IA ON UP.for_value = IA.name AND UP.allow = 'IA'
         LEFT JOIN `tabCBBO` AS CBBO ON UP.for_value = CBBO.name AND UP.allow = 'CBBO'
         LEFT JOIN `tabFPO` AS FPO ON UP.for_value = FPO.name AND UP.allow = 'FPO'
+        LEFT JOIN `tabState` AS TS ON UP.for_value = TS.name AND UP.allow = 'State'
+        LEFT JOIN `tabDistrict` AS TD ON UP.for_value = TD.name AND UP.allow = 'District'
+        LEFT JOIN `tabBlock` AS TB ON UP.for_value = TB.name AND UP.allow = 'Block'
         WHERE UP.user = '{user}'
     """
     return frappe.db.sql(sql_query, as_dict=True)
