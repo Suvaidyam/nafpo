c2b4707b85badb447641bb13f53013b23a7ac784
Samar Raj <samar.suvaidyam@gmail.com>
2024-07-02 15:35:55 +0530

    staff table one way relation

--- nafpo/profiling/doctype/fpo_profiling/fpo_profiling.js
+++ nafpo/profiling/doctype/fpo_profiling/fpo_profiling.js
@@ -1,6 +1,6 @@
 // Copyright (c) 2024, dhwaniris and contributors
 // For license information, please see license.txt
-
+let deleted_staff = [];
 frappe.ui.form.on("FPO Profiling", {
     async refresh(frm) {
         const today = new Date();
@@ -29,6 +29,9 @@ frappe.ui.form.on("FPO Profiling", {
         frm.doc.staff_details_table.forEach(row => {
             // console.log('Row during validate:', row);
         });
+    },
+    before_save(frm) {
+        frm.doc.deleted_staff_rows = deleted_staff;
     },
     state_name: async function (frm) {
         await apply_filter('district_name', 'state', frm, frm.doc.state_name)
@@ -69,5 +72,8 @@ frappe.ui.form.on('FPO Staff Child', {
         console.log(frm)
         let row = frappe.get_doc(cdt, cdn);
         console.log('row :>> ', row);
+    },
+    staff_details_table_remove:function(frm, cdt, cdn){
+        deleted_staff.push(cdn);
     }
 });
