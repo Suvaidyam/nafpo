c9b2d3f7ab7a96de6e218e5ff00ff6201d4c5c50
Abhishek Yadav <abhi.suvaidyam@gmail.com>
2024-07-08 18:40:21 +0530

    completed Crop Module

--- nafpo/crop/doctype/crop_name/crop_name.js
+++ nafpo/crop/doctype/crop_name/crop_name.js
@@ -1,22 +1,39 @@
 // Copyright (c) 2024, dhwaniris and contributors
 // For license information, please see license.txt
 
+let new_entry = false;
 frappe.ui.form.on("Crop Name", {
     refresh(frm) {
-        hide_advance_search(frm, ['state_name', 'name_of_fpo', 'type_of_crop'])
-        extend_options_length(frm, ['state_name', 'name_of_fpo', 'type_of_crop'])
-        apply_filter('name_of_fpo', 'state', frm, frm.doc.state_name)
-        apply_filter('type_of_crop', 'fpo', frm, frm.doc.name_of_fpo)
+        hide_advance_search(frm, ['state_name', 'fpo', 'type_of_crop'])
+        extend_options_length(frm, ['state_name', 'fpo', 'type_of_crop'])
+        apply_filter('fpo', 'state', frm, frm.doc.state_name)
+        apply_filter('type_of_crop', 'single_fpo', frm, frm.doc.fpo)
+    },
+    before_save: function (frm) {
+        if (frm.doc.__islocal) {
+            new_entry = true;
+        } else {
+            new_entry = false;
+        }
+    },
+    after_save: function () {
+        if (new_entry) {
+            change_route();
+        }
     },
     state_name: function (frm) {
-        apply_filter('name_of_fpo', 'state', frm, frm.doc.state_name)
-        truncate_multiple_fields_value(frm, ['name_of_fpo', 'type_of_crop'])
+        apply_filter('fpo', 'state', frm, frm.doc.state_name)
+        truncate_multiple_fields_value(frm, ['fpo', 'type_of_crop'])
     },
-    name_of_fpo: function (frm) {
-        apply_filter('type_of_crop', 'fpo', frm, frm.doc.name_of_fpo)
+    fpo: function (frm) {
+        apply_filter('type_of_crop', 'single_fpo', frm, frm.doc.fpo)
         truncate_multiple_fields_value(frm, ['type_of_crop'])
     },
     onload(frm) {
         hide_list_view_in_useless_data(frm)
     }
 });
+
+var change_route = function () {
+    frappe.set_route("List", "Crop Name");
+}
