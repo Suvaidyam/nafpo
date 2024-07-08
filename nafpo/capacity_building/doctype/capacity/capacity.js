208c56a5375aff8faebd9642205ed8c5b64b508a
Abhishek Yadav <abhi.suvaidyam@gmail.com>
2024-07-08 15:36:44 +0530

    Apply filter in Capacity Building and some change

--- nafpo/capacity_building/doctype/capacity/capacity.js
+++ nafpo/capacity_building/doctype/capacity/capacity.js
@@ -1,33 +1,12 @@
 // Copyright (c) 2024, dhwaniris and contributors
 // For license information, please see license.txt
 
-// async function set_bod_kyc_name(frm) {
-//     if (frm.doc.category === "Governance System (BOD)") {
-//         try {
-//             let response = await callAPI({
-//                 method: "nafpo.apis.api.get_fpo_profile",
-//                 args: {
-//                     name: frm.doc.fpo,
-//                 }
-//             });
-//             if (Array.isArray(response)) {
-//                 let extractedValue = response[0];
-//                 if (extractedValue) {
-//                     frm.set_value('bod_kyc_name', extractedValue);
-//                 } else {
-//                     console.warn('No valid data found in the response.');
-//                 }
-//             } else {
-//                 frm.set_value('bod_kyc_name', response);
-//             }
-//         } catch (error) {
-//             console.error('Error fetching data:', error);
-//         }
-//     }
-// }
-
 frappe.ui.form.on("Capacity", {
     refresh: async function (frm) {
+        apply_filter('fpo_member', 'fpo', frm, frm.doc.fpo)
+        apply_filter('bod_kyc', 'fpo_name', frm, frm.doc.fpo)
+        apply_filter('operation_system', 'fpo', frm, frm.doc.fpo)
+
         frm.image_uploaded = false;
         if (frappe.user.has_role('FPO') && !frappe.user.has_role('Administrator')) {
             try {
@@ -41,6 +20,11 @@ frappe.ui.form.on("Capacity", {
             }
         }
         hide_advance_search(frm, ['fpo_member', 'bod_kyc', 'fpo', 'operation_system'])
+    },
+    fpo(frm) {
+        apply_filter('fpo_member', 'fpo', frm, frm.doc.fpo)
+        apply_filter('bod_kyc', 'fpo_name', frm, frm.doc.fpo)
+        apply_filter('operation_system', 'fpo', frm, frm.doc.fpo)
     },
     category(frm) {
         if (frm.doc.category !== "Membership System (FPO Member)") {
@@ -56,14 +40,14 @@ frappe.ui.form.on("Capacity", {
             frm.set_value('other', '')
         }
     },
+    training_document_if_needed: function (frm) {
+        frm.image_uploaded = true;
+    },
     validate: function (frm) {
         if (frm.image_uploaded) {
             frappe.validated = false;
             frm.image_uploaded = false;
         }
-    },
-    training_document_if_needed: function (frm) {
-        frm.image_uploaded = true;
     },
     start_date(frm) {
         let total_days = Math.ceil(Math.abs(new Date(frm.doc.end_date) - new Date(frm.doc.start_date)) / (1000 * 3600 * 24));
@@ -77,12 +61,6 @@ frappe.ui.form.on("Capacity", {
         frm.set_value('total_days', !isNaN(total_days) && total_days >= 0 ? total_days : 0);
         frm.set_df_property('total_days', 'read_only', true);
     },
-    // ...['training_document_if_needed'].reduce((acc, field) => {
-    //     acc[field] = function (frm) {
-    //         disable_Attachment_autosave(frm);
-    //     };
-    //     return acc;
-    // }, {})
 });
 
 
