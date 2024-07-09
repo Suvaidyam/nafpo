b6448f0971645bb190be2fafd317605c62f18d43
Abhishek Yadav <abhi.suvaidyam@gmail.com>
2024-07-09 16:56:53 +0530

    added Input Side Logic

--- nafpo/business_planning/doctype/business_plannings/business_plannings.py
+++ nafpo/business_planning/doctype/business_plannings/business_plannings.py
@@ -3,6 +3,43 @@ from frappe.model.document import Document
 
 class BusinessPlannings(Document):
     def before_save(self):
+        total_output_income = 0
+        total_input_income = 0
+    # Output Side Logic
+        # Total harvest by FPO members (Quintals)
+        output_side_row = self.output_side
+        for row in output_side_row:
+        # Total harvest by FPO members (Quintals)
+            expected_yields = frappe.db.get_value("Crop Name",row.crop_name,'expected_yields_quintal_per_acre')
+            row.total_harvest_by_fpo_members_quintals = row.total_cropping_area_of_fpo_members_acre * expected_yields
+        # Total Purchase Price 
+            row.total_purchase_price_rs = row.quantity_of_produce_to_be_bought_by_fpo_for_marketing_quintals * row.expected_purchase_pricers
+        # Quantity available for sale (default deducted X% for weight loss)
+            row.quantity_available_for_sale_default_deducted_x_for_weight_loss = (1 - (self.weight_loss_percent / 100)) * row.quantity_of_produce_to_be_bought_by_fpo_for_marketing_quintals
+        # Total selling price/income (Rs)
+            row.total_selling_priceincome_rs = row.quantity_available_for_sale_default_deducted_x_for_weight_loss * row.expected_unit_selling_price_per_quintals
+        # Total Income of FPO from Output
+            row.total_income_of_fpo_from_output = row.total_selling_priceincome_rs * row.total_purchase_price_rs
+        # Total  
+            total_output_income += row.total_income_of_fpo_from_output
+        self.total_income_of_fpo_from_output = total_output_income
+        
+    # Input Side Logic
+        input_side_row = self.input_side
+        for row in input_side_row:
+        # Total Area For Which Input Name Shall be Utilized
+            row.total_area_for_which_input_name_shall_be_utilized = row.total_cropping_area_of_fpo_members_acre * row.percentage_of_total_cropping_area
+        # Total Purchase price (Rs)
+            row.total_purchase_price_rs = row.total_area_for_which_input_name_shall_be_utilized * row.expected_purchase_price_per_acre_rs
+        # Total selling price/income (Rs)
+            row.total_selling_priceincome_rs = row.total_area_for_which_input_name_shall_be_utilized * row.expected_unit_selling_price_per_acre_rs
+        # Total Income of FPO from Input
+            row.total_income_of_fpo_from_input = row.total_selling_priceincome_rs - row.total_purchase_price_rs
+        
+            total_input_income += row.total_income_of_fpo_from_input
+        self.total_income_of_fpo_from_input = total_input_income
+        
+    # Variable Cost Logic
         # Field names and their corresponding rates
         fields_and_rates = {
             'gradingassying_weigning_packingbagging_at_collection_point': 'gradingassying_weigning_packingbagging_at_collection_point_rate',
@@ -26,18 +63,3 @@ class BusinessPlannings(Document):
 
         # Calculate total variable cost
         self.total_variable_cost = sum(getattr(self, field, 0) for field in fields_and_rates)
-        
-        # Output Side
-        # Total harvest by FPO members (Quintals)
-        output_side_row = self.output_side
-        for row in output_side_row:
-            expected_yields = frappe.db.get_value("Crop Name",row.crop_name,'expected_yields_quintal_per_acre')
-            row.total_harvest_by_fpo_members_quintals = row.total_cropping_area_of_fpo_members_acre * expected_yields
-        # Total Purchase Price 
-            row.total_purchase_price_rs = row.quantity_of_produce_to_be_bought_by_fpo_for_marketing_quintals * row.expected_purchase_pricers
-        # Quantity available for sale (default deducted X% for weight loss)
-            row.quantity_available_for_sale_default_deducted_x_for_weight_loss = (1 - (self.weight_loss_percent / 100)) * row.quantity_of_produce_to_be_bought_by_fpo_for_marketing_quintals
-        # Total selling price/income (Rs)
-            row.total_selling_priceincome_rs = row.quantity_available_for_sale_default_deducted_x_for_weight_loss * row.expected_unit_selling_price_per_quintals
-        # Total Income of FPO from Output
-            row.total_income_of_fpo_from_output = row.total_selling_priceincome_rs * row.total_purchase_price_rs
