8c82815c0f5704fb0e6c2674148e742c197820f5
Abhishek Yadav <abhi.suvaidyam@gmail.com>
2024-07-18 12:32:26 +0530

    Some change

--- nafpo/capacity_building/doctype/capacity/capacity.js
+++ nafpo/capacity_building/doctype/capacity/capacity.js
@@ -6,6 +6,7 @@ frappe.ui.form.on("Capacity", {
         apply_filter('fpo_member', 'fpo', frm, frm.doc.fpo)
         apply_filter('bod_kyc', 'fpo_name', frm, frm.doc.fpo)
         apply_filter('operation_system', 'fpo', frm, frm.doc.fpo)
+        frm.fields_dict.end_date.$input.datepicker({ minDate: new Date(frm.doc.start_date) });
 
         frm.image_uploaded = false;
         if (frappe.user.has_role('FPO') && !frappe.user.has_role('Administrator')) {
@@ -18,7 +19,6 @@ frappe.ui.form.on("Capacity", {
                     }
                 });
                 frm.set_value('fpo', fpo.message.fpo);
-                set_due_date(frm);
             } catch (e) {
                 console.error('User data fetch error:', e);
             }
@@ -30,7 +30,32 @@ frappe.ui.form.on("Capacity", {
         apply_filter('bod_kyc', 'fpo_name', frm, frm.doc.fpo)
         apply_filter('operation_system', 'fpo', frm, frm.doc.fpo)
     },
+    fpo_member(frm) {
+        if (frm.doc.category == "Membership System (FPO Member)") {
+            frm.set_value('no_of_participants', frm.doc.fpo_member ? frm.doc.fpo_member.length : 0);
+        }
+    },
+    bod_kyc(frm) {
+        if (frm.doc.category == "Governance System (BOD)") {
+            frm.set_value('no_of_participants', frm.doc.bod_kyc ? frm.doc.bod_kyc.length : 0);
+        }
+    },
+    operation_system(frm) {
+        if (frm.doc.category == "Operation System (CEO/Account/other staff)") {
+            frm.set_value('no_of_participants', frm.doc.operation_system ? frm.doc.operation_system.length : 0);
+        }
+    },
+    other(frm) {
+        if (frm.doc.category == "Other") {
+            if (frm.doc.other && frm.doc.other.length > 0) {
+                frm.set_value('no_of_participants', 1);
+            } else {
+                frm.set_value('no_of_participants', 0);
+            }
+        }
+    },
     category(frm) {
+        frm.set_value('no_of_participants', 0)
         if (frm.doc.category !== "Membership System (FPO Member)") {
             frm.set_value('fpo_member', '')
         }
@@ -44,6 +69,7 @@ frappe.ui.form.on("Capacity", {
             frm.set_value('other', '')
         }
     },
+
     training_document_if_needed: function (frm) {
         frm.image_uploaded = true;
     },
@@ -52,17 +78,24 @@ frappe.ui.form.on("Capacity", {
             frappe.validated = false;
             frm.image_uploaded = false;
         }
+        if (frm.doc.end_date < frm.doc.start_date) {
+            frappe.throw("End Date Can't be grater then Start Date")
+        }
     },
     start_date(frm) {
         let total_days = Math.ceil(Math.abs(new Date(frm.doc.end_date) - new Date(frm.doc.start_date)) / (1000 * 3600 * 24));
         frm.set_value('total_days', !isNaN(total_days) && total_days >= 0 ? total_days : 0);
         frm.set_df_property('total_days', 'read_only', true);
+        frm.fields_dict.end_date.$input.datepicker({ minDate: new Date(frm.doc.start_date) });
     },
     end_date(frm) {
-        frm.fields_dict.end_date.$input.datepicker({ minDate: new Date(frm.doc.start_date) });
         let total_days = Math.ceil(Math.abs(new Date(frm.doc.end_date) - new Date(frm.doc.start_date)) / (1000 * 3600 * 24));
         frm.set_value('total_days', !isNaN(total_days) && total_days >= 0 ? total_days : 0);
         frm.set_df_property('total_days', 'read_only', true);
+        if (frm.doc.end_date < frm.doc.start_date) {
+            frm.set_value('total_days', 0);
+            frappe.throw("End Date Can't be grater then Start Date")
+        }
     },
 });
 
