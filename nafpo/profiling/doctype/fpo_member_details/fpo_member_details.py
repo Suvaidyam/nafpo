129a9a7ae404610545741575fe43e7195534b617
Abhishek Yadav <abhi.suvaidyam@gmail.com>
2024-07-25 11:24:00 +0530

    Update Logic in FPO member details

--- nafpo/profiling/doctype/fpo_member_details/fpo_member_details.py
+++ nafpo/profiling/doctype/fpo_member_details/fpo_member_details.py
@@ -6,4 +6,7 @@ from frappe.model.document import Document
 
 
 class FPOmemberdetails(Document):
-	pass
+	def before_save(self):
+		self.total_owned_in_hectare = self.total_own_land * 0.404686
+		self.total_own_irrigated_land_in_hectare = self.total_own_irrigated_land * 0.404686
+		self.how_much_irrigated_leased_land_in_hectare = self.how_much_irrigated_leased_land_area_in_acre * 0.404686
