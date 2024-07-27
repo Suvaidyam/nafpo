0c2c8d646e0e9c6a62fc6c6d87b978b9d6127fd3
Abhishek Yadav <abhi.suvaidyam@gmail.com>
2024-07-27 12:44:21 +0530

    add

--- nafpo/business_planning/doctype/fpo_fixed_capital/fpo_fixed_capital.py
+++ nafpo/business_planning/doctype/fpo_fixed_capital/fpo_fixed_capital.py
@@ -8,12 +8,12 @@ from frappe.model.document import Document
 
 class FPOFixedCapital(Document):
 	def before_save(self):
-	# 	total_value = 0
-	# 	fixed_capital_details_row = self.fixed_capital_details_table
-	# 	for row in fixed_capital_details_row:
-	# 		total_value += row.value
-	# 	self.total_value = total_value
-	# # Check FPO
+		total_value = 0
+		fixed_capital_details_row = self.fixed_capital_details_table
+		for row in fixed_capital_details_row:
+			total_value += row.value
+		self.total_value = total_value
+	# Check FPO
 		exists = frappe.db.exists({
 				"doctype": "FPO Fixed Capital",
 				"fpo":self.fpo
@@ -22,8 +22,14 @@ class FPOFixedCapital(Document):
 			fpo = frappe.get_doc('FPO',self.fpo)
 			frappe.throw(_(f'{fpo.fpo_name} already exists for the Fpo Fixed Capital'))
 	def on_update(self):
-			frappe.db.set_value('Business Plannings',self.fpo,
-						{
-							'depreciation':self.total_value
-						}
-						,update_modified=False)
+		sfac_ins = frappe.db.exists("Business Plannings", {'fpo': self.fpo})
+		if sfac_ins:
+			# Update Business Plannings document with the total value
+			frappe.db.set_value(
+				'Business Plannings',
+				sfac_ins[0][0],  # Get the name of the document
+				{'depreciation': self.total_value},
+				update_modified=False
+			)
+
+
