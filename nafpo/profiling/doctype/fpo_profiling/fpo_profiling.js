66d62dc3f38b4332a326b6485620efb11f7a042c
Abhishek Yadav <abhi.suvaidyam@gmail.com>
2024-08-05 14:39:10 +0530

    Added validaction in Profiling

--- nafpo/profiling/doctype/fpo_profiling/fpo_profiling.js
+++ nafpo/profiling/doctype/fpo_profiling/fpo_profiling.js
@@ -11,7 +11,6 @@ async function check_exists_fpo(frm) {
         }
     });
     if (response.message) {
-        // frm.set_value('fpo', '')
         return frappe.throw({ message: 'This FPO are already exists in FPO Profiling' });
     }
 }
@@ -21,15 +20,7 @@ frappe.ui.form.on("FPO Profiling", {
         await apply_filter('block_name', 'district', frm, frm.doc.district_name)
         await apply_filter('name_of_the_fpo', 'district', frm, frm.doc.district_name)
         await apply_filter('bod_kyc_name', 'fpo_name', frm, frm.doc.name_of_the_fpo)
-        frm.is_new() ? hide_print_button(frm) : show_print_button(frm);
-        const today = new Date();
-        // frm.fields_dict.date_of_expiry_for_fertilizer.$input.datepicker({ minDate: new Date() });
-        // frm.fields_dict.date_of_incorporation.$input.datepicker({ maxDate: today });
-        // frm.fields_dict.date_of_registration.$input.datepicker({ maxDate: today });
-        // frm.fields_dict.ceo_date_of_joining.$input.datepicker({ maxDate: today });
-        // frm.fields_dict.accountant_date_of_joining.$input.datepicker({ maxDate: today });
-        // frm.fields_dict.ceo_dob.$input.datepicker({ maxDate: new Date(today.getFullYear() - 18, today.getMonth(), today.getDate()) });
-        // frm.fields_dict.accountant_dob.$input.datepicker({ maxDate: new Date(today.getFullYear() - 18, today.getMonth(), today.getDate()) });
+        hide_print_button(frm)
         const expiry_fields = ['date_of_expiry_for_seeds', 'date_of_expiry_for_fertilizer', 'date_of_expiry_for_pesticide', 'date_of_expiry_for_fssai', 'date_of_expiry__for_seed_production'];
         expiry_fields.forEach(field => {
             frm.fields_dict[field].$input.datepicker({ minDate: new Date() });
@@ -41,7 +32,7 @@ frappe.ui.form.on("FPO Profiling", {
     },
     async validate(frm) {
         validate_string(frm, 'fpos_pincode', "FPO's Pincode");
-        validate_string(frm, 'contact_detail_of_fpo', "Contact Detail of FPO");
+        validate_mobile_number(frm.doc.contact_detail_of_fpo)
         if (frm.image_uploaded) {
             frappe.validated = false;
             frm.image_uploaded = false;
@@ -73,7 +64,6 @@ frappe.ui.form.on("FPO Profiling", {
     },
     name_of_the_fpo: async function (frm) {
         await apply_filter('bod_kyc_name', 'fpo_name', frm, frm.doc.name_of_the_fpo)
-        // await apply_filter('block_name', 'fpo_name', frm, frm.doc.name_of_the_fpo);
         truncate_multiple_fields_value(frm, ['bod_kyc_name', 'fpos_address', 'fpos_pincode'])
         await check_exists_fpo(frm)
     },
@@ -86,21 +76,18 @@ frappe.ui.form.on("FPO Profiling", {
 });
 
 frappe.ui.form.on('Supporting Agencies Child', {
-    form_render(frm, cdt, cdn) {
-        // $('.row-actions').remove()
-    },
     from(frm, cdt, cdn) {
         let row = frappe.get_doc(cdt, cdn)
         if (row.from > row.to) {
             row.from = ''
-            frappe.show_alert({ message: "From date should be less than To date", indicator: 'red' })
+            frappe.show_alert({ message: "The 'From' date must be earlier than the 'To' date.", indicator: 'red' })
         }
     },
     to(frm, cdt, cdn) {
         let row = frappe.get_doc(cdt, cdn)
         if (row.from > row.to) {
             row.to = ''
-            frappe.show_alert({ message: "From date should be less than To date", indicator: 'red' })
+            frappe.show_alert({ message: "The 'From' date must be earlier than the 'To' date.", indicator: 'red' })
         }
     }
 });
@@ -123,7 +110,7 @@ frappe.ui.form.on('FPO Staff Child', {
         let row = frappe.get_doc(cdt, cdn);
         if (typeof row.aadhar_no === 'string' && isNaN(Number(row.aadhar_no))) {
             row.aadhar_no = ''
-            frappe.throw({ message: `Please enter a valid Phone No` });
+            frappe.throw({ message: `Please enter a valid Aadhar No` });
         }
     },
     phone_no(frm, cdt, cdn) {
