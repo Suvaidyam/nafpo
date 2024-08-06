b24ff4e4aa0fa1854f5ee35417989b6070cdb36d
Abhishek Yadav <abhi.suvaidyam@gmail.com>
2024-08-06 15:50:03 +0530

    add

--- nafpo/masters/doctype/crops_variety/crops_variety.js
+++ nafpo/masters/doctype/crops_variety/crops_variety.js
@@ -4,7 +4,7 @@
 frappe.ui.form.on("Crops Variety", {
     async refresh(frm) {
         await apply_filter('crops_name', 'crop_type', frm, frm.doc.crops_types)
-        frm.is_new() ? hide_print_button(frm) : show_print_button(frm);
+        hide_print_button(frm)
     },
     crops_types(frm) {
         apply_filter('crops_name', 'crop_type', frm, frm.doc.crops_types)
